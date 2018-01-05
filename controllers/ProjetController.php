5ecd0ae499d23086c732edfafefb7f9f96936790
gna <gnatafsir@gmail.com>
2018-01-05 17:38:24 +0100

    Débute l'implémentation des fonctions gérant l'ajout et la modification de projets

--- controllers/ProjetController.php
+++ controllers/ProjetController.php
@@ -13,19 +13,75 @@ class ProjetController
 
 	function showAddProjectForm()
 	{
+		$chefsProjet = $this->managerContainer->chefProjetManager->getAll();
+
 		include_once(dirname(__DIR__)."/views/AddProjectView.php");
 		return;
 	}
 
 	function addProject()
 	{
-		// Turning the request's parameters into variables for the objects' properties
-		
+		// Retrieving variables from posts parameters
+		$intitule = $_POST['intitule'];
+		$duree = $_POST['duree'];
+		$maitriseOeuvre = $_POST['maitriseOeuvre'];
+		$objet = $_POST['objet'];
+		$dateDemarrage = $_POST['dateDemarrage'];
+		$coucheSI = $_POST['coucheSI'];
+		$cout = $_POST['cout'];
+		$financement = $_POST['financement'];
+		$description = $_POST['description'];
+		//$perspectives = $_POST['perspectives'];
+		//$code = $_POST['code'];
+
+		$chefProjet = $_POST['chefProjet'];
+		$chefProjet = explode(' ', $chefProjet);
+		$chefProjet = $this->managerContainer->chefProjetManager->getOneBy($chefProjet[0], $chefProjet[1]);
+
+		$projet = new Projet();
+
+		$activites_libelle = explode(';', $_POST['activite_libelle']);		
+		$activites_date = explode(';', $_POST['activite_date']);
+		$activites_duree = explode(';', $_POST['activite_duree']);
+
+		for ($i = 0; $i < sizeof($activite_libelle); $i++){
+			$this->managerContainer->activiteManager->add(new Activite(0, $activite_libelle[$i], $activite_date[$i], $activite_duree[$i], $projet));
+		}
+
+		$date = getdate();
+		$date = $date["year"].'-'.$date["mon"].'-'.$date["mday"].' '. $date["hours"].':'.$date["minutes"].':'.$date["seconds"]
+		$log = new Log(0, $date, "creation", $projet);
+		$this->managerContainer->logManager->add($log);
 	}
 
 	function updateProject()
 	{
-		
+		// Retrieving variables from posts parameters
+		$intitule = $_POST['intitule'];
+		$duree = $_POST['duree'];
+		$maitriseOeuvre = $_POST['maitriseOeuvre'];
+		$objet = $_POST['objet'];
+		$dateDemarrage = $_POST['dateDemarrage'];
+		$coucheSI = $_POST['coucheSI'];
+		$cout = $_POST['cout'];
+		$financement = $_POST['financement'];
+		$description = $_POST['description'];
+		//$perspectives = $_POST['perspectives'];
+		//$code = $_POST['code'];
+
+		$chefProjet = $_POST['chefProjet'];
+		$chefProjet = explode(' ', $chefProjet);
+		$chefProjet = $this->managerContainer->chefProjetManager->getOneBy($chefProjet[0], $chefProjet[1]);
+
+		$projet = new Projet();
+
+		$activites_libelle = explode(';', $_POST['activite_libelle']);		
+		$activites_date = explode(';', $_POST['activite_date']);
+		$activites_duree = explode(';', $_POST['activite_duree']);
+
+		for ($i = 0; $i < sizeof($activite_libelle); $i++){
+			$this->managerContainer->activiteManager->add(new Activite(0, $activite_libelle[$i], $activite_date[$i], $activite_duree[$i], $projet));
+		}
 	}
 }
 
